47d21c4b077b8b69a8f936a4834f983c9750a69a
Troy Dorman <troydorman@gmail.com>
2021-09-01 09:21:28 -0600

    re-created getFactAjax with fetch as getFactFetch.  successfully retrieving facts from api

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let factText = document.querySelector("#factText");
 
 let numberInput = document.querySelector("#numberInput");
 
-numberInput.addEventListener("input", getFactAjax);
+numberInput.addEventListener("input", getFactFetch);
 
-function getFactAjax() {
+/* function getFactAjax() {
   let number = numberInput.value;
 
   let xhr = new XMLHttpRequest();
@@ -19,4 +19,15 @@ function getFactAjax() {
   };
 
   xhr.send();
+} */
+
+function getFactFetch() {
+  let number = numberInput.value;
+
+  fetch("http://numbersapi.com/" + number)
+    .then((response) => response.text())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => console.log(err));
 }
