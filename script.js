612a69aaf04275aca2bd3704c820153d8ba05750
Troy Dorman <troydorman@gmail.com>
2021-09-01 09:17:08 -0600

    created getFactAjax() function to make an XMLHttpRequest to our numbers api and fetch a fact.  successfully retrieving fact

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ let numberInput = document.querySelector("#numberInput");
 
 numberInput.addEventListener("input", getFactAjax);
 
+function getFactAjax() {
+  let number = numberInput.value;
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://numbersapi.com/" + number);
+
+  xhr.onload = function () {
+    if (this.status === 200 && number !== "") {
+      console.log(this.responseText);
+    }
+  };
+
+  xhr.send();
+}
